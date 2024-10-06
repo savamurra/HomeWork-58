9d80cc5a9a2f85319b9497319be879c8e11f07d5
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-06 16:05:37 +0600

    Созда дополнительно массив с конфигурациями кнопок которые добавил вниз модального окна. Кнопки имеют свои демонстрационные обработчики запускающие алерт и закрытие модального окна

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import Modal from "./UI/Modal/Modal.tsx";
+import {useState} from "react";
 
-function App() {
+const App = () => {
+    const [showModal, setShowModal] = useState<boolean>(false);
+    const btnArray = [
+        {type: 'primary', label: 'Continue', onClick: () => alert("continue worked")},
+        {type: 'danger', label: 'Close', onClick: () => setShowModal(!showModal)}
+    ]
 
+    const closeModal = () => {
+        setShowModal(!showModal);
+    }
 
-  return (
-    <>
-    </>
-  )
-}
+    return (
+        <>
+            <Modal show={showModal} title="Here was your Order" closeModal={closeModal}>
+                <p className="p-2">Children have any props</p>
+                {btnArray.map((item) => (
+                        <button key={crypto.randomUUID()} className={`btn btn-${item.type} m-2`} onClick={item.onClick}>{item.label}</button>
+                    )
+                )}
+            </Modal>
+            <button className="btn btn-primary m-2" onClick={() => setShowModal(!showModal)}>Modal</button>
+        </>
+    )
+
+};
 
 export default App
