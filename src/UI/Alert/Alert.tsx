10edc410cec0a569d6dcd7eafe1f5af6b5cd6951
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-07 17:02:26 +0600

    Изменил логику для clickDismissabe которая имеет условия что если я передею clickDismissable то кнопка для закрытия не будет появляться

--- src/UI/Alert/Alert.tsx
+++ src/UI/Alert/Alert.tsx
@@ -1,34 +1,58 @@
 import * as React from "react";
+import { motion } from "framer-motion";
 
 interface Props {
-    show: boolean;
-    type: string;
-    closeAlert?: () => void;
-    clickDismissable?: () => void;
+  show: boolean;
+  type: string;
+  closeAlert?: () => void;
+  clickDismissable?: () => void;
 }
 
-const Alert: React.FC<Props> = ({show, closeAlert, type, clickDismissable}) => {
-    return (
-        <>
-            <div className="modal show" style={{display: show ? "block" : "none"}} >
-                <div className="modal-dialog">
-                    <div className={`modal-content bg-${type} border-0`} onClick={clickDismissable}>
-                        <div className="modal-header border-0">
-                            <div className="m-0"><p className="m-0">This is a {type} type alert</p></div>
-                            {closeAlert && (
-                                <button type="button"
-                                        className="btn-close"
-                                        data-bs-dismiss="modal"
-                                        aria-label="Close"
-                                        onClick={closeAlert}>
-                                </button>
-                            )}
-                        </div>
-                    </div>
+const Alert: React.FC<Props> = ({
+  show,
+  closeAlert,
+  type,
+  clickDismissable,
+}) => {
+  return (
+    <>
+      {show && (
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 270, 270, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          }}
+          className="modal show"
+          style={{display: show ? "block" : "none"}}
+        >
+          <div className="modal-dialog">
+            <div
+              className={`modal-content bg-${type} border-0`}
+              onClick={clickDismissable}
+            >
+              <div className="modal-header border-0">
+                <div className="m-0">
+                  <p className="m-0">This is a {type} type alert</p>
                 </div>
+                {clickDismissable
+                  ? null
+                  : closeAlert && (
+                  <button
+                    type="button"
+                    className="btn-close"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                    onClick={closeAlert}
+                  ></button>
+                )}
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </motion.div>
+      )}
+    </>
+  );
 };
 
 export default Alert;
--- src/UI/BackDrop/BackDrop.tsx
+++ src/UI/BackDrop/BackDrop.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 
 interface Props {
-    show: boolean;
+  show: boolean;
 }
-const BackDrop: React.FC<Props> = ({show}) => {
-    return (
-        <div className="modal-backdrop show" style={{display: show ? 'block' : 'none'}}/>
-    );
+const BackDrop: React.FC<Props> = ({ show }) => {
+  return (
+    <div
+      className="modal-backdrop show"
+      style={{ display: show ? "block" : "none" }}
+    />
+  );
 };
 
 export default BackDrop;
